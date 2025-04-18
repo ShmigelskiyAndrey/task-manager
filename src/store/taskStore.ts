5c467e20c87f9e718552304e00b90cb23a935140
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2025-04-18 12:02:54 +0300

    store init

---
+++ src/store/taskStore.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { Task, TaskStatus } from "../types/types";
+
+type TaskStore = {
+  tasks: Task[];
+  addTask: (task: Task) => void;
+  deleteTask: (id: string) => void;
+  updateTask: (updated: Task) => void;
+  moveTask: (id: string, newStatus: TaskStatus) => void;
+}
+
+const useTaskStore = create<TaskStore>()(
+  persist(
+    (set, get) => ({
+      tasks: [],
+      addTask: (task) => set((state) => ({
+        tasks: [...state.tasks, task],
+      })),
+      deleteTask: (id) => set((state) => ({
+        tasks: state.tasks.filter((task) => task.id !== id),
+      })),
+      updateTask: (updated) => set((state) => ({
+        tasks: state.tasks.map((task) => task.id === updated.id ? updated : task ),
+      })),
+      moveTask: (id, newStatus) => set((state) => ({
+        tasks: state.tasks.map((task) => task.id === id ? {...task, status: newStatus} : task),
+      })),
+    }), {
+      name: "task-storage",
+    }
+  )
+);
+
+export default useTaskStore;
